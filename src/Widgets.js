0aed14976aaa5bc0586fc74eaefd51e070a406d6
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-05-22 15:50:26 +0100

    Fixing box-sizing and paddings

--- src/Widgets.js
+++ src/Widgets.js
@@ -4,6 +4,9 @@ import './Widgets.css';
 function Widgets() {
   return (
     <section className="widgetsContainer">
+      <form>
+        <input className="searchTwitter" type="text" placeholder="Search Twitter"/>
+      </form>
       <h2>What’s happening</h2>
     </section>
   );
