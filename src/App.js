6b8ef18fda196060905c74e847d2d848de2a622b
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-05-24 21:35:23 +0100

    Full responsive v1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import './App.css';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
+import { useMediaQuery } from 'react-responsive';
 
 function App() {
+  const hideWidgetsMenu = useMediaQuery({
+    query: '(min-device-width: 1000px)'
+  });
+
+  const mobileWithoutSidebar = useMediaQuery({
+    query: '(min-device-width: 550px)'
+  });
+
   return (
     <div className="app">
       {/* Sidebar */}
-      <Sidebar />
+      {mobileWithoutSidebar && <Sidebar />}
       {/* Feed */}
       <Feed />
 
       {/* Widgets */}
 
-      <Widgets />
+      {hideWidgetsMenu && <Widgets />}
     </div>
   );
 }
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -4,13 +4,21 @@ import { useMediaQuery } from 'react-responsive';
 
 function SidebarOption({ active, text, Icon }) {
   const brakepointDesktop = useMediaQuery({
-    query: '(min-device-width: 1280px)'
+    query: '(min-device-width: 1281px)'
+  });
+
+  const showSidebarMenu = useMediaQuery({
+    query: '(min-device-width: 999px)'
+  });
+
+  const hideSidebarMobile = useMediaQuery({
+    query: '(min-device-width: 750px)'
   });
 
   return (
     <div className={`sidebarOption ${active ? 'sidebarOption--active' : ''}`}>
       <Icon />
-      {brakepointDesktop && <h2>{text}</h2>}
+      {(brakepointDesktop && <h2>{text}</h2>) || (!showSidebarMenu && hideSidebarMobile && <h2>{text}</h2> )}
     </div>
   );
 }
